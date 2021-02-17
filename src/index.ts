760301c32ad11e58f87b0458fcff1b7218e3de85
Akeel Nazir <akeel.mir@capgemini.com>
2021-02-17 12:09:13 +0000

    Printing a user object to console

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hiya');
+import { name, address, setLocale } from 'faker';
+
+setLocale('en_GB');
+
+const person = {
+  firstName: name.firstName(),
+  lastName: name.lastName(),
+};
+
+const _address = {
+  street: address.streetAddress(),
+  city: address.city(),
+  postcode: address.zipCode()
+};
+
+const persona = {
+  fullName: `${person.firstName} ${person.lastName}`,
+  ...person,
+  email: `identity.defra+${person.firstName}.${person.lastName}@gmail.com`,
+  address: _address
+};
+
+console.log(persona);
